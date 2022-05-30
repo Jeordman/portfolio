89e48747c8afb9cf7e5f896100ef7c81ae96af56
jeordman <callisterjeordin@gmail.com>
2022-05-29 22:27:56 -0400

    Dynamic skills section

--- src/components/skills/Skills.svelte
+++ src/components/skills/Skills.svelte
@@ -1,70 +1,46 @@
 <script lang="ts">
-import SkillSection from "./SkillSection.svelte";
-import { Skills } from "./skillText";
+  import { Skills } from './skillText';
 </script>
 
-{#each Skills as skill}
-  <div class="skill-holder">
-    <header class="the-thing">
-      {#each skill.images as image}
-        <img src="{image.src}" alt="{image.alt}" width="{skill.imageWidth}">
+<section id="skills">
+  {#each Skills as skill}
+    <div class="skill-holder">
+      <header class="the-thing">
+        {#each skill.images as image}
+          <img src={image.src} alt={image.alt} width={skill.imageWidth} />
+        {/each}
+      </header>
+      <h2>{skill.title}</h2>
+      {#each skill.info as info}
+        <p>{info}</p>
       {/each}
-    </header>
-    <h2>{skill.title}</h2>
-    {#each skill.info as info}
-      <p>{info}</p>
-    {/each}
-  </div>
-{/each}
-
-  <div>
-    Languages
-
-    <img src={'./images/languages/js.png'} width="28px" />
-    <img src={'./images/languages/go.jpeg'} width="28px" />
-
-    <img src={'./images/languages/ts.jpeg'} width="28px" />
-    <img src={'./images/languages/python.png'} width="28px" />
-    <img src={'./images/languages/php.png'} width="28px" />
-  </div>
-  <div>
-    environment
-    <img src={'./images/languages/nvim.png'} width="28px" />
-    <img
-      src="https://media.giphy.com/media/fh6nwbYdOZ18ObiAf8/giphy.gif"
-      width="160px"
-    />
-  </div>
-
-  <div class="databases">
-    databases
-
-    <img src={'./images/languages/mysql.png'} width="28px" />
-    <img src={'./images/languages/postgres.png'} width="28px" />
-    <img src={'./images/languages/mongo.jpg'} width="28px" />
-  </div>
-
-  <div>
-    Front end
-    <img src={'./images/frameworks/react.png'} width="28px" />
-    <img src={'./images/frameworks/svelte.png'} width="28px" />
-    <img src={'./images/frameworks/vue.webp'} width="28px" />
-  </div>
-
+      {#if skill.example}
+        <h3>{skill.example.title}</h3>
+        <img
+          src={skill.example.image}
+          alt={skill.example.alt}
+          width={skill.example.width}
+        />
+      {/if}
+    </div>
+  {/each}
+</section>
 
 <style>
-.skill-holder{
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin: 0 auto;
-  width: 100%;
-  max-width: 500px;
-  padding: 0 20px;
-}
- .the-thing{
-  
- }
-
+  #skills {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 2rem;
+  }
+  .skill-holder {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 35px;
+    width: 100%;
+    max-width: 500px;
+    padding: 0 20px;
+  }
 </style>
--- src/components/skills/skillText.ts
+++ src/components/skills/skillText.ts
@@ -1,7 +1,7 @@
 export const Skills = [
   {
     title: 'Devops',
-    imageWidth: "80px",
+    imageWidth: '80px',
     images: [
       { src: './images/languages/aws.png', alt: 'aws' },
       { src: './images/languages/docker.webp', alt: 'docker' },
@@ -12,5 +12,61 @@ export const Skills = [
       'I have been using AWS since the beginning of my career',
       'I have been using Docker since the beginning of my career',
     ],
+  },
+  {
+    title: 'Languages',
+    imageWidth: '80px',
+    images: [
+      { src: './images/languages/ts.jpeg', alt: 'ts logo' },
+      { src: './images/languages/go.jpeg', alt: 'docker logo' },
+      { src: './images/languages/php.png', alt: 'php logo' },
+      { src: './images/languages/python.png', alt: 'python logo' },
+      { src: './images/languages/js.png', alt: 'js logo' },
+    ],
+    info: ['Proficient in JavaScript, TypeScript, Go, Python, PHP', 'noice'],
+  },
+  {
+    title: 'Databases',
+    imageWidth: '80px',
+    images: [
+      { src: './images/languages/mysql.png', alt: 'mysql logo' },
+      { src: './images/languages/postgres.png', alt: 'postgres logo' },
+      { src: './images/languages/mongo.jpg', alt: 'mongo logo' },
+    ],
+    info: [
+      'Fully created and managed databases for my projects',
+    ],
+  },
+  {
+    title: 'Web Frameworks/Libraries',
+    imageWidth: '80px',
+    images: [
+      { src: './images/frameworks/react.png', alt: 'reactjs logo' },
+      { src: './images/frameworks/svelte.png', alt: 'sveltejs logo' },
+      { src: './images/frameworks/vue.webp', alt: 'vuejs logo' },
+    ],
+    info: [
+      'Experience in many frontend frameworks',
+    ],
+  },
+  {
+    title: 'Environments',
+    imageWidth: '80px',
+    images: [
+      { src: './images/languages/manjaro.png', alt: 'manjaro logo' },
+      { src: './images/languages/nvim.png', alt: 'nvim logo' },
+      { src: './images/languages/i3.png', alt: 'i3 logo' },
+      { src: './images/languages/tmux.png', alt: 'tmux logo' },
+    ],
+    info: [
+      'I have configured many environments for my projects',
+      'My current setup is Nvim + Tmux + Bash + Manjaro Linux',
+    ],
+    example: {
+      title: 'My current setup',
+      image: 'https://media.giphy.com/media/fh6nwbYdOZ18ObiAf8/giphy.gif',
+      alt: 'gif of current setup',
+      width: '100%',
+    },
   },
 ];
